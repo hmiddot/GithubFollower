10910bce86e2cc25daeadc0e1db27fc9b8ca1a49
Hamidreza Ahmadi <122253551+hmddrz@users.noreply.github.com>
2023-02-23 21:50:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -19,32 +19,31 @@ def follow():
         "Accept": "application/vnd.github.v3+json"
     }
     response = requests.get(
-        f"https://api.github.com/users/{user_to_follow_followers}/followers?per_page=100&page=" + str(j),
+        f"https://api.github.com/users/" + user_to_follow_followers + "/followers?per_page=100&page=" + str(j),
         headers=headers)
     if response.status_code == 200:
         followers = response.json()
         for follower in followers:
             z = str(i + (j - 1) * 100) + "."
-            response = requests.get(f"https://api.github.com/user/following/{follower['login']}", headers=headers)
+            response = requests.get(f"https://api.github.com/user/following/" + follower['login'] + "", headers=headers)
             if response.status_code == 204:
-                print(z + f"You are already following {follower['login']}!")
+                print(z + f"You are already following " + follower['login'] + "!")
             elif response.status_code == 404:
-                response = requests.put(f"https://api.github.com/user/following/{follower['login']}",
+                response = requests.put(f"https://api.github.com/user/following/" + follower['login'] + "",
                                         headers=headers)
                 if response.status_code == 204:
-                    print(z + f"You are now following {follower['login']}!")
+                    print(z + f"You are now following " + follower['login'] + "!")
                 else:
-                    print(z + f"Failed to follow {follower['login']}.")
+                    print(z + f"Failed to follow " + follower['login'] + ".")
             else:
-                print(z + f"Failed to check if you are following {follower['login']}.")
+                print(z + f"Failed to check if you are following " + follower['login'] + ".")
             i += 1
             if i == 101:
                 j += 1
                 follow()
     else:
-        print(f"Failed to retrieve followers for {user_to_follow_followers}.")
+        print(f"Failed to retrieve followers for " + user_to_follow_followers + ".")
 
 
 follow()
 print('Following Ended Successfully !')
-print('Bye !')
