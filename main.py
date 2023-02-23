2a34a6ed31d003a9d0e4aae9e85a809d2e239ecf
Hamidreza Ahmadi <122253551+hmddrz@users.noreply.github.com>
2023-02-23 21:24:17 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+
+
+def main():
+    j = 1
+    i = 1
+    print("Welcome to GithubFollower V.0.1")
+    print("To Start Following Create a Github access-token")
+    username = input("Type Your Own Username : ")
+    access_token = input("Type Your Own PAT : ")
+    user_to_follow_followers = input("Type id of User to Follow : ")
+
+    def follow():
+        global i
+        global j
+        global username
+        global user_to_follow_followers
+        headers = {
+            "Authorization": f"token " + access_token,
+            "Accept": "application/vnd.github.v3+json"
+        }
+        response = requests.get(
+            f"https://api.github.com/users/{user_to_follow_followers}/followers?per_page=100&page=" + str(j),
+            headers=headers)
+        if response.status_code == 200:
+            followers = response.json()
+            for follower in followers:
+                z = str(i + (j - 1) * 100) + "."
+                response = requests.get(f"https://api.github.com/user/following/{follower['login']}", headers=headers)
+                if response.status_code == 204:
+                    print(z + f"You are already following {follower['login']}!")
+                elif response.status_code == 404:
+                    response = requests.put(f"https://api.github.com/user/following/{follower['login']}",
+                                            headers=headers)
+                    if response.status_code == 204:
+                        print(z + f"You are now following {follower['login']}!")
+                    else:
+                        print(z + f"Failed to follow {follower['login']}.")
+                else:
+                    print(z + f"Failed to check if you are following {follower['login']}.")
+                i += 1
+                if i == 101:
+                    j += 1
+                    follow()
+        else:
+            print(f"Failed to retrieve followers for {user_to_follow_followers}.")
+
+    follow()
+    print('Following Ended Successfully !')
+    main()
+
+
+main()
